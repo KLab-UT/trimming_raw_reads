174171a43362bb059bc488fee9ca00ca484c8fdb
Randy Klabacka <u0612750@lonepeak1.wasatch.peaks>
2023-03-03 17:13:21 -0700

    added to trimming script- still not complete

--- bash_scripts/trim_raw_reads.sh
+++ bash_scripts/trim_raw_reads.sh
@@ -27,26 +27,56 @@ echo "load required modules"
 module load fastp/0.20.1
 module load sra-toolkit/3.0.2
 
+echo "create file storing environment"
+mkdir -p sra_files
+mkdir -p raw_reads
+mkdir -p clean_reads
+
 echo "Downloading SRA files from the given list of accessions"
-prefetch --max-size 800G -O ./ --option-file ${l}
+cd sra_files
+prefetch --max-size 800G -O ./ --option-file ../${l}
+ls -p | grep SRR > sra_list
+cd ..
 echo "SRA files were downloaded in current directory"
 echo ""
-ls -p | grep SRR > sra_dirs
-while read i; do mv "$i"*.sra .; done<sra_dirs
-SRA= ls -1 *.sra
-for SRA in *.sra; do fastq-dump --gzip ${SRA}
-done
+
+echo "Getting fastq files from SRA files"
+while read i; do \
+	cd "$i" \
+	fastq-dump --split-files --gzip sra_files/"$i"/"$i".sra \ 
+	# the --split-files option is needed for PE data
+	mv "$i"*.fastq.gz ../raw_reads/ \
+	cd ..
+done<sra_files/sra_list
 echo "Done"
 
-##################################################################################
-# Trimming downloaded Illumina datasets with fastp, using 16 threads (-w option) #
-##################################################################################
+####################################################
+# Trimming downloaded Illumina datasets with fastp #
+####################################################
 
 echo "Trimming downloaded Illumina datasets with fastp."
-z= ls -1 *.fastq.gz
-for z in *.fastq.gz; do fastp -w ${t} -i ${z} -o ${z}.fastp
-gzip ${z}.fastp
-done
+
+ls *.fastq.gz | cut -d "." -f "1" | cut -d "_" -f "1" | sort | uniq > fastq_list
+while read z ; do \
+	cd raw_reads
+	fastqc ${z}_1.fastq.gz
+	fastqc ${z}_2.fastq.gz
+	fastp -i ${z}_1.fastq.gz -I ${z}_2.fastq.gz \ # insert description here
+	-e 25 \ # insert description here
+	-q 15 \ # insert description here
+	-u 40 \ # insert description here
+	-l 15 \ # insert description here
+	--adapter_sequence AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \ # insert description here
+	--adapter_sequence_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \ # insert description here
+	-M 20 -W 4 -5 -3 \ # insert description here
+	-c \ # insert description here
+	-m --merged_out ../cleaned_reads/${z}_merged.fastq \ # insert description here
+	--out1 ../cleaned_reads/${z}_unpaired1_passed.fastq \ # insert description here
+	--out2 ../cleaned_reads/${z}_unpaired2_passed.fastq \ # insert description here
+	--unpaired1 ../cleaned_reads/${z}_unpaired1_failed.fastq \ # insert description here
+	--unpaired2 ../cleaned_reads/${z}_unpaired2_failed.fastq # insert description here 
+gzip ${z}_merged.fastq
+done<fastq_list
 echo ""
 
 
