4c2c349eae7836436c657c4b7cf31fd4b61e8aaf
Randy Klabacka <u0612750@lonepeak2.wasatch.peaks>
2023-03-03 21:52:55 -0700

    updated trim_raw_reads.sh to first complete draft- running it now on CHPC. Created worksheet.md with questions

--- bash_scripts/trim_raw_reads.sh
+++ bash_scripts/trim_raw_reads.sh
@@ -24,13 +24,16 @@ fi
 begin=`date +%s`
 
 echo "load required modules"
-module load fastp/0.20.1
+module load fastqc/0.11.4
+module load multiqc/1.12
 module load sra-toolkit/3.0.2
+module load fastp/0.20.1
 
 echo "create file storing environment"
 mkdir -p sra_files
 mkdir -p raw_reads
-mkdir -p clean_reads
+mkdir -p cleaned_reads/merged_reads
+mkdir -p cleaned_reads/unmerged_reads
 
 echo "Downloading SRA files from the given list of accessions"
 cd sra_files
@@ -41,24 +44,40 @@ echo "SRA files were downloaded in current directory"
 echo ""
 
 echo "Getting fastq files from SRA files"
+cd sra_files
 while read i; do \
 	cd "$i" \
-	fastq-dump --split-files --gzip sra_files/"$i"/"$i".sra \ 
+	fastq-dump --split-files --gzip "$i".sra \ 
 	# the --split-files option is needed for PE data
-	mv "$i"*.fastq.gz ../raw_reads/ \
+	mv "$i"*.fastq.gz ../../raw_reads/ \
 	cd ..
-done<sra_files/sra_list
+done<sra_list
+cd ..
 echo "Done"
+
+
+###################################
+# Quality check of raw read files #
+###################################
+
+echo "Perform quality check of raw read files"
+cd raw_reads
+pwd
+while read i; do \
+	fastqc "$i"_1.fastq.gz # insert description here
+	fastqc "$i"_2.fastq.gz # insert description here
+done<../sra_files/sra_list
+multiqc . # insert description here
 
 ####################################################
 # Trimming downloaded Illumina datasets with fastp #
 ####################################################
 
 echo "Trimming downloaded Illumina datasets with fastp."
-
+cd raw_reads
+pwd
 ls *.fastq.gz | cut -d "." -f "1" | cut -d "_" -f "1" | sort | uniq > fastq_list
 while read z ; do \
-	cd raw_reads
 	fastqc ${z}_1.fastq.gz
 	fastqc ${z}_2.fastq.gz
 	fastp -i ${z}_1.fastq.gz -I ${z}_2.fastq.gz \ # insert description here
@@ -70,15 +89,30 @@ while read z ; do \
 	--adapter_sequence_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \ # insert description here
 	-M 20 -W 4 -5 -3 \ # insert description here
 	-c \ # insert description here
-	-m --merged_out ../cleaned_reads/${z}_merged.fastq \ # insert description here
-	--out1 ../cleaned_reads/${z}_unpaired1_passed.fastq \ # insert description here
-	--out2 ../cleaned_reads/${z}_unpaired2_passed.fastq \ # insert description here
-	--unpaired1 ../cleaned_reads/${z}_unpaired1_failed.fastq \ # insert description here
-	--unpaired2 ../cleaned_reads/${z}_unpaired2_failed.fastq # insert description here 
-gzip ${z}_merged.fastq
+	-m --merged_out ../cleaned_reads/merged_reads/${z}_merged.fastq \ # insert description here
+	--out1 ../cleaned_reads/unmerged_reads/${z}_unpaired1_passed.fastq \ # insert description here
+	--out2 ../cleaned_reads/unmerged_reads/${z}_unpaired2_passed.fastq \ # insert description here
+	--unpaired1 ../cleaned_reads/unmerged_reads/${z}_unpaired1_failed.fastq \ # insert description here
+	--unpaired2 ../cleaned_reads/unmerged_reads/${z}_unpaired2_failed.fastq # insert description here 
+        cd ../cleaned_reads/merged_reads
+	gzip ${z}_merged.fastq
+	cd ../../raw_reads
 done<fastq_list
+cd ..
 echo ""
 
 
+
+#######################################
+# Quality check of cleaned read files #
+#######################################
+
+echo "Perform check of cleaned read files"
+cd cleaned_reads/merged_reads
+pwd
+while read i; do \
+	fastqc "$i"_merged.fastq.gz # insert description here
+done<../sra_files/sra_list
+multiqc . # insert description here
 
 }
--- worksheet.md
+++ worksheet.md
@@ -1,1 +1,76 @@
-IN PROGRESS
+# Trimming Raw Reads Worksheet
+
+<!--- Write name below --->
+## Name: 
+
+<!--- For this worksheet, answer the following questions--->
+
+## Q1: What does "cleaning" your reads mean?
+Answer:
+
+### Q2: Open the script called "trim_raw_reads.sh". For every line that says ```insert description here```, replace that text with a description of what the line will do
+
+### Q4: Attach an image of the plot from fastqc showing the average base quality from your reads files
+![before-trimming-PHRED-plot-1](./images/<plot-for-reads1>)
+![before-trimming-PHRED-plot-1](./images/<plot-for-reads2>)
+
+---
+
+## The following questions pertain to your first fastp run (without altering the parameters)
+### Q5: How many reads were there in the R1 file before filtering?
+Answer:
+### Q6: How many bases were there in the R1 file before filtering?
+Answer:
+### Q7: What proportion of bases were above PHRED score of 20 before filtering?
+Answer: 
+### Q8: What proportion of bases were above PHRED score of 30 before filtering?
+Answer: 
+
+### Q9: How many reads were there in the R1 file after filtering?
+Answer:
+### Q10: How many bases were there in the R1 file after filtering?
+Answer:
+### Q11: What proportion of bases were above PHRED score of 20 after filtering?
+Answer: 
+### Q12: What proportion of bases were above PHRED score of 30 after filtering?
+Answer: 
+
+
+### Q13: How many reads were there in the R2 file before filtering?
+Answer:
+### Q14: How many bases were there in the R2 file before filtering?
+Answer:
+### Q15: What proportion of bases were above PHRED score of 20 before filtering?
+Answer: 
+### Q16: What proportion of bases were above PHRED score of 30 before filtering?
+Answer: 
+
+### Q17: How many reads were there in the R2 file after filtering?
+Answer:
+### Q18: How many bases were there in the R2 file after filtering?
+Answer:
+### Q19: What proportion of bases were above PHRED score of 20 after filtering?
+Answer: 
+### Q20: What proportion of bases were above PHRED score of 30 after filtering?
+Answer: 
+
+---
+
+## The following questions pertain to Remix 1 (the first time you change fastp parameters)
+### Q21: What parameters did you change?
+Answer: 
+### Q22: How did you expect this to change the filtering results (be specific)?
+Answer: 
+### Q23: Explain the results. Did the change cause an effect that matched your expectations? Use information from the fastp output to explain.
+Answer: 
+
+---
+
+## The following questions pertain to Remix 2 (the first time you change fastp parameters)
+### Q24: What parameters did you change?
+Answer: 
+### Q25: How did you expect this to change the filtering results (be specific)?
+Answer: 
+### Q26: Explain the results. Did the change cause an effect that matched your expectations? Use information from the fastp output to explain.
+Answer: 
+
